4708c8cdd12d977454e8d0c3381f8e075c9cfb2c
Hussam Abd <hussam3bd@gmail.com>
2019-08-30 13:55:22 +0300

    fix footer on mobile

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,7 @@
 		<div class="row align-items-start">
 			<div class="col-lg-8 col-sm-12">
 				<div class="row">
-					<nav class="nav mx-auto mx-md-0 mx-lg-0 justify-content-sm-center justify-content-md-start">
+					<nav class="nav mx-auto mx-md-0 mx-lg-0 justify-content-center justify-content-md-start">
 						<a class="nav-item nav-link" href="{{ route('pages.about') }}" title="عن لينكاتي">
 							عن لينكاتي
 						</a>
@@ -27,14 +27,14 @@
 				</p>
 			</div>
 			<div class="col-lg-4 col-sm-12">
-				<nav class="nav justify-content-sm-center justify-content-md-end">
-					<a href="https://twitter.com/linkatiapp" target="_blank"
-					   rel="nofollow noopener noreferrer" title="Twitter" class="nav-link nav-item">
-						<i class="fab fa-twitter"></i>
-					</a>
+				<nav class="nav justify-content-center justify-content-md-end">
 					<a href="https://instagr.am/linkatiapp" target="_blank"
 					   rel="nofollow noopener noreferrer" title="Instagram" class="nav-link nav-item">
 						<i class="fab fa-instagram"></i>
+					</a>
+					<a href="https://twitter.com/linkatiapp" target="_blank"
+					   rel="nofollow noopener noreferrer" title="Twitter" class="nav-link nav-item">
+						<i class="fab fa-twitter"></i>
 					</a>
 					<a href="https://fb.com/linkatiapp" target="_blank" rel="nofollow noopener noreferrer"
 					   title="Facebook" class="nav-link nav-item">
