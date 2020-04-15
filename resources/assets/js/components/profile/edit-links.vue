4d7108a7a22f78ef3c367e6989b88973c290102d
Hussam Abd <hussam3bd@gmail.com>
2020-04-15 22:39:12 +0300

    show clicks for all users

--- resources/assets/js/components/profile/edit-links.vue
+++ resources/assets/js/components/profile/edit-links.vue
@@ -1,69 +1,71 @@
 <template>
-	<div class="card">
-		<div class="card-header font-weight-600">
-			تخصيص الروابط
-		</div>
-		<div class="card-body">
-			<create-link></create-link>
+    <div class="card">
+        <div class="card-header font-weight-600">
+            تخصيص الروابط
+        </div>
+        <div class="card-body">
+            <create-link></create-link>
 
-			<draggable tag="ul" :list="profile.links" class="list-group pr-0 mt-4" handle=".handle" @sort="resort">
-				<li class="list-group-item"
-				    v-for="(link, idx) in profile.links"
-				    :key="idx">
-					<a class="btn btn-primary btn-lg w-100 text-right" data-toggle="collapse" :href="'#link-'+ link.id">
-						<i class="fas fa-sort handle" style="padding: 20px;margin: -8px -16px -8px 0;"></i>
-						<i class="mr-2" :class="link.icon"></i>
-						<span>{{ link.name }}</span>
+            <draggable tag="ul" :list="profile.links" class="list-group pr-0 mt-4" handle=".handle" @sort="resort">
+                <li class="list-group-item"
+                    v-for="(link, idx) in profile.links"
+                    :key="idx">
+                    <a class="btn btn-primary btn-lg w-100 text-right" data-toggle="collapse" :href="'#link-'+ link.id">
+                        <i class="fas fa-sort handle" style="padding: 20px;margin: -8px -16px -8px 0;"></i>
+                        <i class="mr-2" :class="link.icon"></i>
+                        <span>{{ link.name }}</span>
 
-						<small class="float-left mt-2 badge badge-secondary font-weight-500"
-						       title="عدد النقرات"
-						       data-toggle="tooltip"
-						       data-placement="right"
-						       v-show="profile.user_id === 1 || profile.user_id === 43">
-							{{link.clicks}}
-						</small>
-					</a>
-					<div class="collapse" :id="'link-'+ link.id">
-						<div class="card">
-							<div class="card-body">
-								<div class="form-group">
-									<label class="font-weight-600">
-										العنوان
-										<small class="text-danger">*</small>
-									</label>
-									<input type="text" v-model="link.name" placeholder="العنوان الظاهر على الزر" name="name"
-									       class="form-control" :class="{'is-invalid': errors.name}" required>
-									<div class="invalid-feedback" v-if="errors.name">{{errors.name[0]}}</div>
-								</div>
-								<div class="form-group">
-									<label class="font-weight-600">
-										الرابط
-										<small class="text-danger">*</small>
-									</label>
-									<input type="url" name="url" dir="ltr" v-model="link.url" placeholder="لينك الزر"
-									       class="form-control" :class="{'is-invalid': errors.url}" required>
-									<div class="invalid-feedback" v-if="errors.url">{{errors.url[0]}}</div>
-								</div>
-							</div>
-							<div class="card-footer">
-								<button class="btn btn-outline-danger" @click="deleteLink(link.id, idx)" :disabled="submiting">
-									<i class="fas fa-spinner fa-spin" v-if="submiting"></i>
-									حذف اللينك
-								</button>
-								<button class="btn btn-secondary float-left" @click="updateLink(link)" :disabled="submiting">
-									<i class="fas fa-spinner fa-spin" v-if="submiting"></i>
-									حفظ التعديلات
-								</button>
-							</div>
-						</div>
-					</div>
-				</li>
-			</draggable>
+                        <small class="float-left mt-2 badge badge-secondary font-weight-500"
+                               title="عدد النقرات"
+                               data-toggle="tooltip"
+                               data-placement="right">
+                            {{link.clicks}}
+                        </small>
+                    </a>
+                    <div class="collapse" :id="'link-'+ link.id">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label class="font-weight-600">
+                                        العنوان
+                                        <small class="text-danger">*</small>
+                                    </label>
+                                    <input type="text" v-model="link.name" placeholder="العنوان الظاهر على الزر"
+                                           name="name"
+                                           class="form-control" :class="{'is-invalid': errors.name}" required>
+                                    <div class="invalid-feedback" v-if="errors.name">{{errors.name[0]}}</div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="font-weight-600">
+                                        الرابط
+                                        <small class="text-danger">*</small>
+                                    </label>
+                                    <input type="url" name="url" dir="ltr" v-model="link.url" placeholder="لينك الزر"
+                                           class="form-control" :class="{'is-invalid': errors.url}" required>
+                                    <div class="invalid-feedback" v-if="errors.url">{{errors.url[0]}}</div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button class="btn btn-outline-danger" @click="deleteLink(link.id, idx)"
+                                        :disabled="submiting">
+                                    <i class="fas fa-spinner fa-spin" v-if="submiting"></i>
+                                    حذف اللينك
+                                </button>
+                                <button class="btn btn-secondary float-left" @click="updateLink(link)"
+                                        :disabled="submiting">
+                                    <i class="fas fa-spinner fa-spin" v-if="submiting"></i>
+                                    حفظ التعديلات
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </draggable>
 
-			<pre dir="ltr" class="text-left bg-light p-4 mt-2"
-			     v-if="env != 'production'">{{profile.links}}</pre>
-		</div>
-	</div>
+            <pre dir="ltr" class="text-left bg-light p-4 mt-2"
+                 v-if="env != 'production'">{{profile.links}}</pre>
+        </div>
+    </div>
 </template>
 
 <script>
