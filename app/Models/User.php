174f7d6d5f63f61ed284494a000be87c9cb544cb
khaled3afan <khaled3afan@gmail.com>
2021-10-22 11:28:44 +0300

    test git

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable implements MustVerifyEmail
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * The attributes that should be cast to native types..
      *
      * @var array
      */
